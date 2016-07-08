c97ebf1d4bb332564fb7558201c08bc94c00a4af
raphaelchang <raphaelchang1227@gmail.com>
2016-07-08 01:50:11 -0700

    More console work

--- client/client.js
+++ client/client.js
@@ -37,16 +37,105 @@ $(function() {
 	$('.ui.dropdown')
 	  .dropdown()
 	;
-	$("#console").bind('keydown', function(e) {
-		$("#cursor").replaceWith($("#cursor")); // Freeze cursor blinking when typing
-		if (e.which === 8)
+	var history = [];
+	var inputReset = '<span>resc>&nbsp;</span><span id="input"><span class="cursor">&nbsp;</span></span>';
+	var historyIndex = 0;
+	var currentCommand;
+	var cursor = $(".cursor");
+	$("#console").bind("keypress", function(e) {
+		$(".cursor").replaceWith($(".cursor")); // Freeze cursor blinking when typing
+		console.log(e.which);
+		e.preventDefault();
+		if (e.which == 32)
 		{
-			$("#console #input").text($("#console #input").text().substring(0, $("#console #input").text().length - 1))
+			$('<span />').html("&nbsp;").insertBefore(cursor);
 		}
 		else
 		{
-			$("#console #input").append(String.fromCharCode(e.which));
+			$('<span />').html(String.fromCharCode(e.keyCode)).insertBefore(cursor);
 		}
-		e.preventDefault();
+	});
+	$("#console").bind("keydown", function(e) {
+		console.log(e.which);
+		$(".cursor").replaceWith($(".cursor")); // Freeze cursor blinking when typing
+		if (e.keyCode == 8) {
+			cursor.prev().remove();
+			e.preventDefault();
+		}
+		else if (e.ctrlKey && e.which == 67)
+		{
+			$("#cmd").html(inputReset);
+			cursor = $(".cursor");
+			historyIndex = 0;
+			e.preventDefault();
+		}
+		else if (e.which == 13)
+		{
+			cursor.removeClass('cursor');
+			console.log($("#cmd #input").html());
+			if ($("#cmd #input").html() != '<span class="">&nbsp;</span>')
+			{
+				history.push($("#cmd #input").html());
+			}
+			$("#output").append($("#cmd").html());
+			$("#output").append("<br />");
+			$("#cmd").html(inputReset);
+			$("#console").scrollTop(999999999);
+			cursor = $(".cursor");
+			historyIndex = 0;
+			e.preventDefault();
+		}
+		else if (e.which == 37)
+		{
+			var temp = cursor.prev();
+			if (temp.is('span'))
+			{
+				cursor.removeClass("cursor");
+				cursor = temp;
+				cursor.addClass("cursor");
+			}
+			e.preventDefault();
+		}
+		else if (e.which == 39)
+		{
+			var temp = cursor.next();
+			if (temp.is('span'))
+			{
+				cursor.removeClass("cursor");
+				cursor = temp;
+				cursor.addClass("cursor");
+			}
+			e.preventDefault();
+		}
+		else if (e.which == 38)
+		{
+			if (historyIndex < history.length)
+			{
+				if (historyIndex == 0)
+				{
+					currentCommand = $("#cmd #input").html();
+				}
+				historyIndex++;
+				$("#cmd #input").html(history[history.length - historyIndex]);
+				cursor = $("#cmd #input").children().last();
+				cursor.addClass("cursor");
+			}
+			e.preventDefault();
+		}
+		else if (e.which == 40)
+		{
+			if (historyIndex > 0)
+			{
+				if (historyIndex == 1)
+				{
+					$("#cmd #input").html(currentCommand);
+				}
+				historyIndex--;
+				$("#cmd #input").html(history[history.length - historyIndex]);
+				cursor = $("#cmd #input").children().last();
+				cursor.addClass("cursor");
+			}
+			e.preventDefault();
+		}
 	});
 });
